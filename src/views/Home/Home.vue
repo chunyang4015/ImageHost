82ed6d5598a0b590dc06ece6e0c6bedd2de8cecd
chunyang <chunyang@chunyangs-MacBook-Pro.local>
2026-06-12 17:04:38 +0800

    feat: add clear history button when upload records exist

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -3,12 +3,16 @@
     <!-- 上传 -->
     <Upload v-model="fileList" :UploadConfig="UploadConfig" :uploadAPI="uploadAPI" />
     <!-- 展示 -->
+    <div v-if="fileList.length" class="clear-bar">
+      <Button variant="outline" size="sm" @click="clearHistory">清空历史</Button>
+    </div>
     <ResList v-model="fileList" :nodeHost="nodeHost" />
   </section>
 </template>
 <script setup lang="ts">
 import { ref, watch } from 'vue';
 import { formatURL } from '@/utils/index';
+import { Button } from '@/components/ui/button';
 import Upload from '@/components/Upload/Upload.vue';
 import ResList from '@/components/ResList/ResList.vue';
 // IPFS节点
@@ -23,6 +27,12 @@ const UploadConfig = ref<any>({
 });
 // 上传列表
 const fileList = ref<Array<any>>(JSON.parse(localStorage.getItem('zychUpImageList') || '[]'));
+
+function clearHistory() {
+  fileList.value = []
+  localStorage.removeItem('zychUpImageList')
+}
+
 watch(fileList, (newVal) => {
   localStorage.setItem(
     'zychUpImageList',
@@ -40,4 +50,9 @@ watch(fileList, (newVal) => {
 
 <style scoped lang="less">
 @import 'Home.less';
+.clear-bar {
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 0.5rem;
+}
 </style>
